c30dca95952fef11980fa1d574ef9a0d5e4f3112
Greenway-Benjamin <benjamingreenway@gmail.com>
2019-11-17 00:38:59 -0700

    MVC App

---
+++ MvcMovie/MvcMovie/Models/Genre.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcMovie.Models
+{
+    public class Genre
+    {
+        public Genres MovieGenre { get; set; }
+    }
+
+    public enum Genres
+    {
+        Action,
+        Drama,
+        Faith,
+        Comedy
+    }
+}
